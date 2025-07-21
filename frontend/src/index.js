57622965ee134064090eb35704a90f3b8f5acd70
Vishakha Kumari <166810633+Vishakhakumari17@users.noreply.github.com>
2025-07-21 17:47:05 +0530

    Updated frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,75 +1,9 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
-const Signup = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: ''
-  });
-
-  const [message, setMessage] = useState('');
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post(
-        `${import.meta.env.VITE_BACKEND_URL}/signup`,
-        formData
-      );
-      setMessage('Signup successful!');
-    } catch (err) {
-      console.error(err);
-      setMessage('Signup failed.');
-    }
-  };
-
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Signup</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="username"
-          onChange={handleChange}
-          placeholder="Username"
-          required
-        />
-        <br />
-        <input
-          name="email"
-          type="email"
-          onChange={handleChange}
-          placeholder="Email"
-          required
-        />
-        <br />
-        <input
-          name="password"
-          type="password"
-          onChange={handleChange}
-          placeholder="Password"
-          required
-        />
-        <br />
-        <button type="submit">Signup</button>
-      </form>
-      <p>{message}</p>
-    </div>
-  );
-};
-
-
-
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
-import Signup from "./landing_page/signup/Signup";
+import Signup from './landing_page/signup/Signup';
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
@@ -96,6 +30,3 @@ root.render(
     <Footer />
   </BrowserRouter>
 );
-
-
-export default Signup;
